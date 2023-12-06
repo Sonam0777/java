b131b0d84bda7a0cc34201e6dcc6dd15a3b32937
Sonam <sonammzyou@gmail.com>
2023-12-06 21:02:06 +0545

    my first git program

---
+++ class1/output.java
@@ -0,0 +1,7 @@
+class output{
+    public static void main(String args[]){
+        System.out.println("Hello World.");
+        System.out.println("my name is sonam sherpa.");
+        System.out.print("i am 21 years old.");
+    }
+}
