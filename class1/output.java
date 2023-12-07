cf737c614b24221529083b503af14055371a64ac
Sonam <sonammzyou@gmail.com>
2023-12-07 08:16:05 +0545

    some message

--- class1/output.java
+++ class1/output.java
@@ -1,5 +1,5 @@
-class output{
-    public static void main(String args[]){
+public class output{
+    public static void main(String[] args){
         System.out.println("Hello World.");
         System.out.println("my name is sonam sherpa.");
         System.out.print("i am 21 years old.");
